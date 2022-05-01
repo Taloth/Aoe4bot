8222a54f7528a4738393cd19d6b471e045a3b4eb
Taloth Saldono <Taloth@users.noreply.github.com>
2022-05-01 01:05:18 +0100

    Bumped node to v16 and reduced log chatter on liveliness probe

--- index.js
+++ index.js
@@ -33,16 +33,18 @@ function wrapError(handler) {
 
 function logRequest(req, res, next)
 {
-  const nightbotUser = new URLSearchParams(req.headers['nightbot-user'] || '').get('displayName');
-  const nightbotChannel = new URLSearchParams(req.headers['nightbot-channel'] || '').get('displayName');
-  const by = nightbotChannel ? ` [by ${nightbotUser}@${nightbotChannel}]` : '';
-  const startTime = performance.now();
-  console.log(`Api Req: ${req.url}${by}`);
-  res.once('finish', () => {
-    const endTime = performance.now();
-    const elapsed = endTime - startTime;
-    console.log(`Api Res: ${req.url} (${res.statusCode}, ${elapsed.toFixed(0)} msec)`);
-  });
+  if (req.url != '/') {
+    const nightbotUser = new URLSearchParams(req.headers['nightbot-user'] || '').get('displayName');
+    const nightbotChannel = new URLSearchParams(req.headers['nightbot-channel'] || '').get('displayName');
+    const by = nightbotChannel ? ` [by ${nightbotUser}@${nightbotChannel}]` : '';
+    const startTime = performance.now();
+    console.log(`Api Req: ${req.url}${by}`);
+    res.once('finish', () => {
+      const endTime = performance.now();
+      const elapsed = endTime - startTime;
+      console.log(`Api Res: ${req.url} (${res.statusCode}, ${elapsed.toFixed(0)} msec)`);
+    });
+  }
   next();
 }
 
